e8e1d6e5bd12b562bbb59e0f0c3adcb9aabb7c19
Yichen Xu <yichen.x@outlook.com>
2021-11-23 20:27:25 +0800

    Add alternative definition of constraint interpretation

--- src/constr-dot/ConstrLangAlt.v
+++ src/constr-dot/ConstrLangAlt.v
@@ -248,62 +248,110 @@ Definition tctx := env typ.
 Definition vctx := env var.
 
 Reserved Notation "e '⊧' C" (at level 40).
-Reserved Notation "es '⊢t' T1 '::' T2" (at level 40, T1 at level 59, T2 at level 59).
-Reserved Notation "es '⊢d' d1 '::' d2" (at level 40, d1 at level 59, d2 at level 59).
-Reserved Notation "es '⊢v' x '::' y" (at level 40, x at level 59, y at level 59).
+Reserved Notation "es '⊢t' T1 '⪯' T2" (at level 40, T1 at level 59, T2 at level 59).
+Reserved Notation "es '⊢d' d1 '⪯' d2" (at level 40, d1 at level 59, d2 at level 59).
+Reserved Notation "es '⊢v' x '⪯' y" (at level 40, x at level 59, y at level 59).
+Reserved Notation "es '⊢vv' x '⪯' y" (at level 40, x at level 59, y at level 59).
+Reserved Notation "es '⊢vd' x '⪯' y" (at level 40, x at level 59, y at level 59).
+Reserved Notation "es '⊢vds' x '⪯' y" (at level 40, x at level 59, y at level 59).
 
-Inductive map_cvar_to : vctx -> cvar -> var -> Prop :=
+Inductive map_cvar : vctx -> cvar -> var -> Prop :=
 | map_cvar_f : forall vm x y,
     binds x y vm ->
-    map_cvar_to vm (cvar_f x) y
+    map_cvar vm (cvar_f x) y
 | map_cvar_x : forall vm x,
-    map_cvar_to vm (cvar_x x) x
+    map_cvar vm (cvar_x x) x
 .
 
-Inductive map_ctyp_to : (tctx * vctx) -> ctyp -> typ -> Prop :=
+Inductive map_ctyp : (tctx * vctx) -> ctyp -> typ -> Prop :=
 
-| map_top_to : forall tm vm,
-    (tm, vm) ⊢t ctyp_top :: typ_top
+| map_ctyp_top : forall tm vm,
+    (tm, vm) ⊢t ctyp_top ⪯ typ_top
 
-| map_bot_to : forall tm vm,
-    (tm, vm) ⊢t ctyp_bot :: typ_bot
+| map_ctyp_bot : forall tm vm,
+    (tm, vm) ⊢t ctyp_bot ⪯ typ_bot
 
-| map_tvar_to : forall tm vm x T,
+| map_ctyp_tvar : forall tm vm x T,
     binds x T tm ->
-    (tm, vm) ⊢t ctyp_tvar (tvar_f x) :: T
+    (tm, vm) ⊢t ctyp_tvar (tvar_f x) ⪯ T
 
-| map_rcd_to : forall tm vm D D',
-    (tm, vm) ⊢d D :: D' ->
-    (tm, vm) ⊢t ctyp_rcd D :: typ_rcd D'
+| map_ctyp_rcd : forall tm vm D D',
+    (tm, vm) ⊢d D ⪯ D' ->
+    (tm, vm) ⊢t ctyp_rcd D ⪯ typ_rcd D'
 
-| map_and_to : forall tm vm T T' U U',
-    (tm, vm) ⊢t T :: T' ->
-    (tm, vm) ⊢t U :: U' ->
-    (tm, vm) ⊢t ctyp_and T U :: typ_and T' U'
+| map_ctyp_and : forall tm vm T T' U U',
+    (tm, vm) ⊢t T ⪯ T' ->
+    (tm, vm) ⊢t U ⪯ U' ->
+    (tm, vm) ⊢t ctyp_and T U ⪯ typ_and T' U'
 
-| map_sel_to : forall tm vm x y T,
-    map_cvar_to vm x y ->
-    (tm, vm) ⊢t ctyp_sel x T :: typ_sel (avar_f y) T
+| map_ctyp_sel : forall tm vm x y T,
+    map_cvar vm x y ->
+    (tm, vm) ⊢t ctyp_sel x T ⪯ typ_sel (avar_f y) T
 
-| map_bnd_to : forall tm vm T T',
-    (tm, vm) ⊢t T :: T' ->
-    (tm, vm) ⊢t ctyp_bnd T :: typ_bnd T'
+| map_ctyp_bnd : forall tm vm T T',
+    (tm, vm) ⊢t T ⪯ T' ->
+    (tm, vm) ⊢t ctyp_bnd T ⪯ typ_bnd T'
 
-| map_all_to : forall tm vm T T' U U',
-    (tm, vm) ⊢t T :: T' ->
-    (tm, vm) ⊢t U :: U' ->
-    (tm, vm) ⊢t ctyp_all T U :: typ_all T' U'
+| map_ctyp_all : forall tm vm T T' U U',
+    (tm, vm) ⊢t T ⪯ T' ->
+    (tm, vm) ⊢t U ⪯ U' ->
+    (tm, vm) ⊢t ctyp_all T U ⪯ typ_all T' U'
 
-where "es '⊢t' T1 '::' T2" := (map_ctyp_to es T1 T2)
-with map_cdec_to : (tctx * vctx) -> cdec -> dec -> Prop :=
-| map_cdec_typ_to : forall tm vm A S S' T T',
-    (tm, vm) ⊢t S :: S' ->
-    (tm, vm) ⊢t T :: T' ->
-    (tm, vm) ⊢d cdec_typ A S T :: dec_typ A S' T'
-| map_cdec_trm_to : forall tm vm a T T',
-    (tm, vm) ⊢t T :: T' ->
-    (tm, vm) ⊢d cdec_trm a T :: dec_trm a T'
-where "es '⊢d' D1 '::' D2" := (map_cdec_to es D1 D2).
+where "es '⊢t' T1 '⪯' T2" := (map_ctyp es T1 T2)
+with map_cdec : (tctx * vctx) -> cdec -> dec -> Prop :=
+| map_cdec_typ : forall tm vm A S S' T T',
+    (tm, vm) ⊢t S ⪯ S' ->
+    (tm, vm) ⊢t T ⪯ T' ->
+    (tm, vm) ⊢d cdec_typ A S T ⪯ dec_typ A S' T'
+| map_cdec_trm : forall tm vm a T T',
+    (tm, vm) ⊢t T ⪯ T' ->
+    (tm, vm) ⊢d cdec_trm a T ⪯ dec_trm a T'
+where "es '⊢d' D1 '⪯' D2" := (map_cdec es D1 D2).
+
+Inductive map_ctrm : (tctx * vctx) -> ctrm -> trm -> Prop :=
+| map_ctrm_cvar : forall tm vm x y,
+    map_cvar vm x y ->
+    (tm, vm) ⊢v ctrm_cvar x ⪯ trm_var (avar_f y)
+| map_ctrm_val : forall tm vm v v',
+    map_cval (tm, vm) v v' ->
+    (tm, vm) ⊢v ctrm_val v ⪯ trm_val v'
+where "es '⊢v' t1 '⪯' t2" := (map_ctrm es t1 t2)
+with map_cval : (tctx * vctx) -> cval -> val -> Prop :=
+| map_cval_new : forall tm vm T T' ds ds',
+    (tm, vm) ⊢t T ⪯ T' ->
+    (tm, vm) ⊢vds ds ⪯ ds' ->
+    (tm, vm) ⊢vv cval_new T ds ⪯ val_new T' ds'
+| map_cval_lambda : forall tm vm T T' t t',
+    (tm, vm) ⊢t T ⪯ T' ->
+    (tm, vm) ⊢v t ⪯ t' ->
+    (tm, vm) ⊢vv cval_lambda T t ⪯ val_lambda T' t'
+where "es '⊢vv' t1 '⪯' t2" := (map_cval es t1 t2)
+with map_cdef : (tctx * vctx) -> cdef -> def -> Prop :=
+| map_cdef_typ : forall tm vm A T T',
+    (tm, vm) ⊢t T ⪯ T' ->
+    (tm, vm) ⊢vd cdef_typ A T ⪯ def_typ A T'
+| map_cdef_trm : forall tm vm a t t',
+    (tm, vm) ⊢v t ⪯ t' ->
+    (tm, vm) ⊢vd cdef_trm a t ⪯ def_trm a t'
+where "es '⊢vd' t1 '⪯' t2" := (map_cdef es t1 t2)
+with map_cdefs : (tctx * vctx) -> cdefs -> defs -> Prop :=
+| map_cdefs_nil : forall tm vm,
+    (tm, vm) ⊢vds cdefs_nil ⪯ defs_nil
+| map_cdefs_cons : forall tm vm ds ds' d d',
+    (tm, vm) ⊢vds ds ⪯ ds' ->
+    (tm, vm) ⊢vd d ⪯ d' ->
+    (tm, vm) ⊢vds cdefs_cons ds d ⪯ defs_cons ds' d'
+where "es '⊢vds' t1 '⪯' t2" := (map_cdefs es t1 t2).
+
+Scheme map_ctyp_mut    := Induction for map_ctyp Sort Prop
+with   map_cdec_mut    := Induction for map_cdec Sort Prop.
+Combined Scheme map_ctyp_mutind from map_ctyp_mut, map_cdec_mut.
+
+Scheme map_ctrm_mut     := Induction for map_ctrm Sort Prop
+with   map_cval_mut     := Induction for map_cval Sort Prop
+with   map_cdef_mut     := Induction for map_cdef Sort Prop
+with   map_cdefs_mut    := Induction for map_cdefs Sort Prop.
+Combined Scheme map_ctrm_mutind from map_ctrm_mut, map_cval_mut, map_cdef_mut, map_cdefs_mut.
 
 Inductive satisfy_constr : (tctx * vctx * ctx) -> constr -> Prop :=
 
@@ -331,16 +379,156 @@ Inductive satisfy_constr : (tctx * vctx * ctx) -> constr -> Prop :=
     (forall x, x \notin L -> (tm, vm & x ~ u, G) ⊧ C ^^v u) ->
     (tm, vm, G) ⊧ (∃v C)
 
-(* | sat_typ : forall tm vm G t t' T T', *)
-(*     ctrm_closed t t' -> *)
-(*     ctyp_closed T T' -> *)
-(*     G ⊢ t' : T' -> *)
-(*     G ⊧ t ⦂ T *)
+| sat_typ : forall tm vm G t t' T T',
+    (tm, vm) ⊢v t ⪯ t' ->
+    (tm, vm) ⊢t T ⪯ T' ->
+    G ⊢ t' : T' ->
+    (tm, vm, G) ⊧ t ⦂ T
 
 | sat_sub : forall tm vm G S S' T T',
-    (tm, vm) ⊢t S :: S' ->
-    (tm, vm) ⊢t T :: T' ->
+    (tm, vm) ⊢t S ⪯ S' ->
+    (tm, vm) ⊢t T ⪯ T' ->
     G ⊢ S' <: T' ->
     (tm, vm, G) ⊧ S <⦂ T
 
 where "e '⊧' C" := (satisfy_constr e C).
+
+Hint Constructors satisfy_constr constr.
+
+(** * Constraint Entailment *)
+
+(** ** Definition of entailment *)
+Definition constr_entail (C1 C2 : constr) :=
+  forall tm vm G,
+    inert G ->
+    (tm, vm, G) ⊧ C1 -> (tm, vm, G) ⊧ C2.
+
+Notation "C '⊩' D" := (constr_entail C D) (at level 50).
+
+(** ** Tactics *)
+
+Ltac introe := introv H0 H.
+
+Ltac inv_sat :=
+  match goal with
+  | H : _ ⊧ (_ _) |- _ => idtac H; inversion H; subst; clear H
+  | H : _ ⊧ (_ _ _) |- _ => idtac H; inversion H; subst; clear H
+  | H : _ ⊧ ⊥ |- _ => idtac H; inversion H; subst; clear H
+  end.
+
+Ltac inv_sat_all := repeat inv_sat.
+
+(** ** Entailment Laws *)
+(** ∀ C, ⊥ ⊩ C
+    From false follows everything. *)
+Theorem ent_absurd : forall C,
+    ⊥ ⊩ C.
+Proof.
+  introe. inversion H.
+Qed.
+
+(** ∀ C, C ⊩ ⊤
+    Obtaining True is trivial. *)
+Theorem ent_tautology : forall C,
+    C ⊩ ⊤.
+Proof. introe. eauto. Qed.
+
+(** ∀ C, C ⊩ C *)
+Lemma ent_refl : forall C,
+    C ⊩ C.
+Proof.
+  introe. auto.
+Qed.
+
+(** If C1 ⊩ C2 and C2 ⊩ C3, then C1 ⊩ C3. *)
+Theorem ent_trans : forall C1 C2 C3,
+    C1 ⊩ C2 ->
+    C2 ⊩ C3 ->
+    C1 ⊩ C3.
+Proof.
+  introv H12 H23.
+  introe.
+  eauto.
+Qed.
+
+Theorem ent_cong_and : forall C C' D,
+    C ⊩ C' ->
+    C ⋏ D ⊩ C' ⋏ D.
+Proof.
+  introv He. introe. inv_sat.
+  eauto.
+Qed.
+
+Theorem ent_and_comm : forall C D,
+    C ⋏ D ⊩ D ⋏ C.
+Proof.
+  introe. inv_sat. eauto.
+Qed.
+
+Theorem ent_and_left : forall C D,
+    C ⋏ D ⊩ C.
+Proof. introe. inversion H; subst. eauto. Qed.
+
+Theorem ent_and_right : forall C D,
+    C ⋏ D ⊩ D.
+Proof. introe. inversion H; subst. eauto. Qed.
+
+Theorem ent_and_intro : forall C D,
+    C ⊩ D ->
+    C ⊩ C ⋏ D.
+Proof.
+  introv Hcd. introe.
+  constructor*.
+Qed.
+
+Lemma ent_and_assoc : forall C1 C2 C3,
+    (C1 ⋏ C2) ⋏ C3 ⊩ C1 ⋏ (C2 ⋏ C3).
+Proof.
+  introe. inv_sat. inv_sat. eauto.
+Qed.
+
+Lemma ent_or_comm : forall C D,
+    C ⋎ D ⊩ D ⋎ C.
+Proof.
+  introe. inv_sat; eauto.
+Qed.
+
+Lemma ent_or_assoc : forall C1 C2 C3,
+    (C1 ⋎ C2) ⋎ C3 ⊩ C1 ⋎ (C2 ⋎ C3).
+Proof.
+  introe. inv_sat; try inv_sat. all: eauto.
+Qed.
+
+Lemma ent_or_true : forall C,
+    ⊤ ⊩ C ⋎ ⊤.
+Proof.
+  introe. eauto.
+Qed.
+
+Lemma ent_or_false : forall C,
+    C ⋎ ⊥ ⊩ C.
+Proof.
+  introe. inv_sat; eauto. inv_sat.
+Qed.
+
+Lemma ent_or_dist_and : forall C D1 D2,
+    C ⋎ (D1 ⋏ D2) ⊩ (C ⋎ D1) ⋏ (C ⋎ D2).
+Proof.
+  introe. inv_sat; try inv_sat; eauto.
+Qed.
+
+Lemma ent_and_dist_or : forall C D1 D2,
+    C ⋏ (D1 ⋎ D2) ⊩ (C ⋏ D1) ⋎ (C ⋏ D2).
+Proof.
+  introe. inv_sat; try inv_sat; eauto.
+Qed.
+
+(** If C ⊩ D, then ∃ x. C ⊩ ∃ x. D *)
+Lemma ent_cong_exists_v : forall C D,
+    C ⊩ D ->
+    ∃v C ⊩ ∃v D.
+Proof.
+  introv Hent. introe. inv_sat.
+  apply sat_exists_var with (u := u) (L := L).
+  apply* Hent.
+Qed.

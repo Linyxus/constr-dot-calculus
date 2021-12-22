c3dfaeeb398e19617ecf9fc599dee5ced49904eb
Yichen Xu <yichen.x@outlook.com>
2021-12-23 01:13:33 +0800

    Well-typed programs

--- src/constr-dot/ConstrSafety.v
+++ src/constr-dot/ConstrSafety.v
@@ -11,3 +11,13 @@ Require Import ConstrWeakening ConstrNarrowing.
 Require Import ConstrBinding.
 Require Import InvertibleConstrTyping.
 Require Import ConstrCanonicalForms.
+
+(** The typing of a term with a stack *)
+Inductive constr_sta_trm_typ : sta * trm -> typ -> Prop :=
+| constr_sta_trm_typ_c : forall G s t T,
+    inert G ->
+    G ⊨ C ->
+    well_typed G s ->
+    G ⊢ t : T ->
+    sta_trm_typ (s, t) T.
+

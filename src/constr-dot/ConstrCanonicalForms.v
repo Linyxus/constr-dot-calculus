38162afb84271231ec47bf8738b85826198aab8e
Yichen Xu <yichen.x@outlook.com>
2021-12-23 01:35:30 +0800

    Finish canonical lemma

--- src/constr-dot/ConstrCanonicalForms.v
+++ src/constr-dot/ConstrCanonicalForms.v
@@ -209,6 +209,78 @@ Proof.
 Qed.
 Hint Resolve constr_well_typed_to_ok_G.
 
+Lemma constr_well_typed_to_ok_s: forall s C G,
+    constr_well_typed C G s -> ok s.
+Proof.
+  intros. destruct H as [? [? ?]]. auto.
+Qed.
+Hint Resolve constr_well_typed_to_ok_s.
+
+(** [s: G]       #<br>#
+    [x ∉ dom(G)] #<br>#
+    [――――――――――] #<br>#
+    [x ∉ dom(s)] *)
+Lemma constr_well_typed_notin_dom: forall C G s x,
+    constr_well_typed C G s ->
+    x # s ->
+    x # G.
+Proof.
+  introv Hwt. destruct Hwt as [? [? [?Hdom ?]]].
+  unfold notin. rewrite Hdom.
+  auto.
+Qed.
+
+Lemma constr_well_typed_empty: forall C,
+    constr_well_typed C empty empty.
+Proof.
+  repeat split; auto.
+  - simpl_dom; auto.
+  - introv B. exfalso; apply* binds_empty_inv.
+Qed.
+Hint Resolve constr_well_typed_empty.
+
+Lemma constr_well_typed_push: forall C G s x T v,
+    constr_well_typed C G s ->
+    x # G ->
+    x # s ->
+    (C, G) ⊢c trm_val v : T ->
+    constr_well_typed C (G & x ~ T) (s & x ~ v).
+Proof.
+  intros. unfold constr_well_typed in *.
+  destruct_all.
+  repeat split; auto.
+  - simpl_dom. fequal. auto.
+  - intros x0 T0 v0 BxG. gen v0.
+    destruct (binds_push_inv BxG) as [[?Heqx ?HeqT] | [?Hneq ?HB]].
+    + subst x0 T0. introv Bxv. apply binds_push_eq_inv in Bxv.
+      subst v0. apply weaken_cty_trm; auto.
+    + intros.
+      assert (binds x0 T0 G) by eauto using binds_push_neq_inv.
+      assert (binds x0 v0 s) by eauto using binds_push_neq_inv.
+      apply weaken_cty_trm; eauto.
+Qed.
+Hint Resolve constr_well_typed_push.
+
+(** [s: G]              #<br>#
+    [G(x) = T]          #<br>#
+    [―――――――――――――]     #<br>#
+    [exists v, s(x) = v]     #<br>#
+    [G ⊢ v: T]          *)
+Lemma constr_corresponding_types: forall C G s x T,
+    constr_well_typed C G s ->
+    binds x T G ->
+    (exists v, binds x v s /\
+          (C, G) ⊢c trm_val v : T).
+Proof.
+  introv Hwt BiG. destruct Hwt as [?HokG [?HokS [?HdomEq ?]]].
+  pose proof (get_some_inv BiG) as HinDom.
+  symmetry in HdomEq.
+  pose proof (get_some (Logic.eq_ind_r _ HinDom HdomEq)) as [?v Bis].
+  exists v. split.
+  - apply Bis.
+  - eauto.
+Qed.
+
 (** * Canonical Forms for Functions
 
     [inert G]            #<br>#
@@ -228,16 +300,41 @@ Lemma constr_canonical_forms_fun: forall C G s x T U,
 Proof.
   introv Hin Hsat Hwt Hty.
   destruct (constr_var_typ_all_to_binds Hin Hsat Hty) as [L [S [T' [BiG [Hs1 Hs2]]]]].
-  destruct (corresponding_types Hwt BiG) as [v [Bis Ht]].
-  destruct (val_typ_all_to_lambda Hin Ht) as [L' [S' [t [Heq [Hs1' Hs2']]]]].
+  destruct (constr_corresponding_types Hwt BiG) as [v [Bis Ht]].
+  destruct (constr_val_typ_all_to_lambda Hin Hsat Ht) as [L' [S' [t [Heq [Hs1' Hs2']]]]].
   subst.
   exists (L \u L' \u (dom G)) S' t. repeat split~.
-  - eapply subtyp_trans; eauto.
+  - eapply constr_subtyping_trans; eauto.
   - intros.
     assert (HL: y \notin L) by auto.
     assert (HL': y \notin L') by auto.
     specialize (Hs2 y HL).
     specialize (Hs2' y HL').
-    apply narrow_typing with (G':=G & y ~ T) in Hs2'; auto.
-    + eapply ty_sub; eauto.
+    apply narrow_constr_typing with (G':=G & y ~ T) in Hs2'; auto.
+    + eapply cty_sub; eauto.
+Qed.
+
+(** * Canonical Forms for Objects
+
+    [inert G]            #<br>#
+    [s: G]               #<br>#
+    [G ⊢ x: {a:T}]       #<br>#
+    [――――――――――――――――――] #<br>#
+    [exists S, ds, t,] #<br>#
+    [s(x) = nu(S)ds] #<br>#
+    [ds^x = ... /\ {a = t} /\ ...] #<br>#
+    [G ⊢ t: T] *)
+Lemma constr_canonical_forms_obj: forall C G s x a T,
+  inert G ->
+  G ⊨ C ->
+  constr_well_typed C G s ->
+  (C, G) ⊢c trm_var (avar_f x) : typ_rcd (dec_trm a T) ->
+  (exists S ds t, binds x (val_new S ds) s /\ defs_has (open_defs x ds) (def_trm a t) /\ (C, G) ⊢c t : T).
+Proof.
+  introv Hi Hsat Hwt Hty.
+  destruct (constr_var_typ_rcd_to_binds Hi Hsat Hty) as [S [T' [Bi [Hr Hs]]]].
+  destruct (constr_corresponding_types Hwt Bi) as [v [Bis Ht]].
+  apply cty_var with (C:=C) in Bi. apply cty_rec_elim in Bi.
+  destruct (constr_val_mu_to_new Hi Hsat Ht Bi Hr) as [t [ds [Heq [Hdefs Ht']]]].
+  subst. exists S ds t. repeat split~. eapply cty_sub; eauto.
 Qed.

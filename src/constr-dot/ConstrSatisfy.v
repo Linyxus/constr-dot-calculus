42e7a2ccae605c20dc1b8c8dd06fe84a5cac2822
Yichen Xu <yichen.x@outlook.com>
2022-01-03 12:24:06 +0800

    Update constr satisfy theorems

--- src/constr-dot/ConstrSatisfy.v
+++ src/constr-dot/ConstrSatisfy.v
@@ -42,3 +42,45 @@ Inductive simple_formed_constr : constr -> Prop :=
 
 .
 
+Lemma simple_constr_empty_mapping : forall tm vm G C,
+    simple_formed_constr C ->
+    (tm, vm, G) ⊧ C ->
+    (empty, empty, G) ⊧ C.
+Proof.
+  introv Hsf Hsat.
+  dependent induction Hsf; eauto; inversions Hsat; eauto.
+  - lets Heqs: (map_iso_ctyp tm vm H).
+    lets Heqt: (map_iso_ctyp tm vm H0).
+    lets Heqs1: (map_ctyp_unique_typ H5 Heqs).
+    lets Heqt1: (map_ctyp_unique_typ H7 Heqt).
+    subst. clear Heqs Heqt.
+    lets Heqs: (map_iso_ctyp empty empty H).
+    lets Heqt: (map_iso_ctyp empty empty H0).
+    eauto.
+  - lets Heqt: (map_iso_ctyp tm vm H).
+    lets Heqt1: (map_ctyp_unique_typ H6 Heqt).
+    clear Heqt.
+    subst.
+    lets Heqt: (map_iso_ctyp empty empty H).
+    inversions H4. inversions H5.
+    eapply sat_typ; try eassumption.
+    constructor. constructor.
+Qed.
+
+Lemma subst_satisfy_aux1 : forall x y T G C,
+    simple_formed_constr C ->
+    binds x T G ->
+    y # G ->
+    (empty, empty, G) ⊧ C ->
+    (empty, empty, G & y ~ T) ⊧ subst_constr x y C.
+Proof.
+  introv Hsf Hb HyG Hsat.
+  dependent induction Hsf; eauto.
+  - inversions Hsat.
+  - inversions Hsat. simpl. eauto.
+  - inversions Hsat; simpl; eauto.
+  - admit.
+  - inversions Hsat. inversions H4. inversions H5.
+    simpl. cases_if.
+    --
+
